08f9149296a347fd900842abd35678f247597086
Ben Ranker <branker@emory.edu>
2009-11-16 22:34:05 +0000

    basic distutils support
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/trunk@8 1168706f-7e1d-44e7-b746-9e172b5a9a2f

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import sys
+
+sys.path.append('src')
+from eulcore import __version__
+
+setup(
+    name='eulcore',
+    version=__version__,
+    author='Emory University Libraries',
+    author_email='libsysdev-l@listserv.cc.emory.edu',
+    packages=('eulcore',),
+    package_dir={'': 'src'}
+)
