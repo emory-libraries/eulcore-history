3b22a391298f0f6e4ac03a0420e01061c5dbd18f
Rebecca Sutton Koeser <rsutton@emory.edu>
2010-01-15 18:22:48 +0000

    move xmlmap into a module
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/trunk@23 1168706f-7e1d-44e7-b746-9e172b5a9a2f

---
+++ src/eulcore/xmlmap/__init__.py
@@ -0,0 +1,2 @@
+# import core xmlmap components so they can be used as eulcore.xmlmap
+from eulcore.xmlmap.core import XmlObject,XPathNode,XPathNodeList,XPathString,XPathStringList,XPathInteger,XPathIntegerList,XPathDate,parseUri,parseString
