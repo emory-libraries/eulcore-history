b36965db8fc42f6d9595332581f97b779a2ce282
Ben Ranker <branker@emory.edu>
2010-02-10 22:58:58 +0000

    split FedoraTestCase into new test_fedora.base module in preparation for non-server test code
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/trunk@54 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- test/test_fedora/test_server.py
+++ test/test_fedora/test_server.py
@@ -1,44 +1,7 @@
 #!/usr/bin/env python
 
-import os
 import unittest
-from eulcore.fedora.server import Repository
-
-REPO_ROOT = 'https://dev11.library.emory.edu:8643/fedora/'
-REPO_USER = 'fedoraAdmin'
-REPO_PASS = 'fedoraAdmin'
-
-FIXTURE_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fixtures')
-
-class FedoraTestCase(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        unittest.TestCase.__init__(self, *args, **kwargs)
-        self.fedora_fixtures_ingested = []
-
-    def setUp(self):
-        self.repo = Repository(REPO_ROOT, REPO_USER, REPO_PASS)
-        fixtures = getattr(self, 'fixtures', [])
-        for fixture in fixtures:
-            self.ingestFixture(fixture)
-
-    def tearDown(self):
-        for pid in self.fedora_fixtures_ingested:
-            self.repo.purge_object(pid)
-
-    def loadFixtureData(self, fname):
-        fname = os.path.join(FIXTURE_ROOT, fname)
-        with open(fname) as f:
-            return f.read()
-
-    def ingestFixture(self, fname):
-        object = self.loadFixtureData(fname)
-        pid = self.repo.ingest(object)
-        if pid:
-            # we'd like this always to be true. if ingest fails we should
-            # throw an exception. that probably hasn't been thoroughly
-            # tested yet, though, so we'll check it until it has been.
-            self.fedora_fixtures_ingested.append(pid)
-
+from test_fedora.base import FedoraTestCase
 
 class TestBasicFedoraFunctionality(FedoraTestCase):
     def testGetNextPID(self):
