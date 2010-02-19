5a0b8b255d51c3d6ff24a4042fbb0832b673b023
Rebecca Sutton Koeser <rsutton@emory.edu>
2010-02-19 20:41:36 +0000

    flesh out EAD xmlmap object fields; better testing #9, #3
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/trunk@63 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- src/eulcore/xmlmap/eadmap.py
+++ src/eulcore/xmlmap/eadmap.py
@@ -1,9 +1,117 @@
 from eulcore import xmlmap
 
-class ead(xmlmap.XmlObject):
-    """xmlmap object for an EAD (Encoded Archival Description) Finding Aid"""
-    title 	= xmlmap.XPathString('eadheader/filedesc/titlestmt/titleproper')
-    author 	= xmlmap.XPathString('eadheader/filedesc/titlestmt/author')
-    unittitle 	= xmlmap.XPathString('archdesc[@level="collection"]/did/unittitle')
+# xmlmap objects for various sections of an ead
+# organized from smallest/lowest level to highest level
+
+class Section(xmlmap.XmlObject):
+    # generic section - head and paragraph
+    head   = xmlmap.XPathString("head")
+    content = xmlmap.XPathStringList("p")       # ??
+
+class Heading(xmlmap.XmlObject):
+    """Generic xml object for headings used under controlaccess"""
+    source = xmlmap.XPathString("@source")
+    value  = xmlmap.XPathString(".")
+
+    def __str__(self):
+        return self.value
+
+class ControlledAccessHeadings(Section):
+    """controlaccess element - controlled access headings"""
+    person_name = xmlmap.XPathNodeList("persname", Heading)
+    family_name = xmlmap.XPathNodeList("famname", Heading)
+    corporate_name = xmlmap.XPathNodeList("corpname", Heading)
+    subject = xmlmap.XPathNodeList("subject", Heading)
+    geographic_name = xmlmap.XPathNodeList("geogname", Heading)
+    genre_form = xmlmap.XPathNodeList("genreform", Heading)
+    occupation = xmlmap.XPathNodeList("occupation", Heading)
+    function = xmlmap.XPathNodeList("function", Heading)
+    title = xmlmap.XPathNodeList("title", Heading)
+    # catch-all to get any of these, in order
+    terms = xmlmap.XPathNodeList("corpname|famname|function|genreform|geogname|occupation|persname|subject|title", Heading)
+
+# recursive mapping - currently has to be declared after class has been defined
+ControlledAccessHeadings.controlaccess = xmlmap.XPathNodeList("controlaccess", ControlledAccessHeadings)
+
+class Container(xmlmap.XmlObject):
+    """container element - did subelement for locating materials"""
+    type = xmlmap.XPathString("@type")
+    value = xmlmap.XPathString(".")
+
+    def __str__(self):
+        return self.value
+
+class DescriptiveIdentification(xmlmap.XmlObject):
+    """did element - Descriptive Information for materials in a component"""
+    unitid = xmlmap.XPathString("unitid")
+    unittitle = xmlmap.XPathString("unittitle")
+    unitdate = xmlmap.XPathString("unitdate")
+    physdesc = xmlmap.XPathString("physdesc")
+    container = xmlmap.XPathNodeList("container", Container)
+
+class Component(xmlmap.XmlObject):
+    """generic cN (c1-c12) element - a subordinate component of the materials"""
+    level = xmlmap.XPathString("@level")
+    did = xmlmap.XPathNode("did", DescriptiveIdentification)
+    # using un-numbered mapping for c-series or container lists
+    def hasSubseries(self):
+        """check if this component has subseries"""        
+        if self.c and self.c[0] and ((self.c[0].level in ('series', 'subseries')) or
+            (self.c[0].c and self.c[0].c[0])):            
+            return True
+        else:
+            return False
+
+# another recursive mapping - currently has to be declared after class has been defined
+Component.c = xmlmap.XPathNodeList("c02|c03|c04|c05|c06|c07|c08|c09|c10|c11|c12", Component)
+
+class SubordinateComponents(Section):
+    """dsc element - Description of Subordinate Components; container lists and series"""
+    type = xmlmap.XPathString("@type")
+    c = xmlmap.XPathNodeList("c01", Component)
+    
+    def hasSeries(self):
+        """check if this finding aid has series/subseries"""
+        if self.c[0].level == 'series' or (self.c[0].c and self.c[0].c[0]):
+            return True
+        else:
+            return False
+
+class ArchivalDescription(xmlmap.XmlObject):
+    """archdesc element, Archival description; makes up the bulk of an EAD document"""
+    origination = xmlmap.XPathString("did/origination")
+    unitid = xmlmap.XPathString("did/unitid")
+    extent = xmlmap.XPathString("did/physdesc/extent")
+    langmaterial = xmlmap.XPathString("did/langmaterial")
+    location = xmlmap.XPathString("did/physloc")
+    access_restriction = xmlmap.XPathNode("accessrestrict", Section)
+    use_restriction = xmlmap.XPathNode("userestrict", Section)
+    alternate_form = xmlmap.XPathNode("altformavail", Section)
+    originals_location = xmlmap.XPathNode("originalsloc", Section)
+    related_material = xmlmap.XPathNode("relatedmaterial", Section)
+    separated_material = xmlmap.XPathNode("separatedmaterial", Section)
+    acquisition_info = xmlmap.XPathNode("acqinfo", Section)
+    custodial_history = xmlmap.XPathNode("custodhist", Section)
+    preferred_citation = xmlmap.XPathNode("prefercite", Section)
+    biography_history = xmlmap.XPathNode("bioghist", Section)
+    bibliography = xmlmap.XPathNode("bibliography", Section)
+    scope_content  = xmlmap.XPathNode("scopecontent", Section)
+    arrangement = xmlmap.XPathNode("arrangement", Section)
+    other = xmlmap.XPathNode("otherfindaid", Section)
+    controlaccess = xmlmap.XPathNode("controlaccess", ControlledAccessHeadings)
+
+class EncodedArchivalDescription(xmlmap.XmlObject):
+    """xmlmap object for an Encoded Archival Description (EAD) Finding Aid"""
+    id = xmlmap.XPathString('@id')
+    eadid = xmlmap.XPathString('eadheader/eadid')
+    # mappings for fields common to access or display as top-level information
+    title = xmlmap.XPathString('eadheader/filedesc/titlestmt/titleproper')
+    author = xmlmap.XPathString('eadheader/filedesc/titlestmt/author')
+    unittitle = xmlmap.XPathString('archdesc[@level="collection"]/did/unittitle')
     physical_desc = xmlmap.XPathString('archdesc[@level="collection"]/did/physdesc')
-    abstract    = xmlmap.XPathString('archdesc[@level="collection"]/did/abstract')
+    abstract = xmlmap.XPathString('archdesc[@level="collection"]/did/abstract')
+
+    archdesc  = xmlmap.XPathNode("archdesc", ArchivalDescription)
+    # dsc is under archdesc, but is a major section - mapping at top-level for convenience
+    dsc = xmlmap.XPathNode("archdesc/dsc", SubordinateComponents)
+
--- test/test_xmlmap/test_ead.py
+++ test/test_xmlmap/test_ead.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python
+
 import unittest
 from os import path
 
-from eulcore.xmlmap  import load_xmlobject_from_file, XPathString
-from eulcore.xmlmap.eadmap import ead
+from eulcore.xmlmap  import load_xmlobject_from_file, load_xmlobject_from_string
+#from eulcore.xmlmap.eadmap import EncodedArchivalDescription, ArchivalDescription, ControlledAccessHeadings, Section, Heading, SubordinateComponents, Component
+from eulcore.xmlmap.eadmap import *
 
 class TestEad(unittest.TestCase):
     FIXTURE_FILE = path.join(path.dirname(path.abspath(__file__)) ,
                              'fixtures', 'heaney653.xml')
     def setUp(self):
-        self.ead = load_xmlobject_from_file(self.FIXTURE_FILE, ead)
+        self.ead = load_xmlobject_from_file(self.FIXTURE_FILE, EncodedArchivalDescription)
 
     def testInit(self):
-        self.assert_(isinstance(self.ead, ead))
+        self.assert_(isinstance(self.ead, EncodedArchivalDescription))
 
     def testBasicFields(self):
         self.assertEqual(self.ead.title, "Seamus Heaney collection, 1972-1997")
+        self.assertEqual(self.ead.eadid, "heaney653.xml")
+        self.assertEqual(self.ead.id, "heaney653-011")
         self.assertEqual(self.ead.author, "Manuscript, Archives, and Rare Book Library, Emory University")
         # whitespace makes fields with tags a bit messier...
         self.assert_("Seamus Heaney collection," in self.ead.unittitle)
@@ -24,6 +29,130 @@ class TestEad(unittest.TestCase):
         self.assert_("(3 boxes)" in self.ead.physical_desc)
         self.assert_("12 oversized papers (OP)" in self.ead.physical_desc)
         self.assert_("materials relating to Irish poet Seamus Heaney" in self.ead.abstract)
+
+    def test_ArchivalDescription(self):
+        self.assert_(isinstance(self.ead.archdesc, ArchivalDescription))
+        ad = self.ead.archdesc
+        self.assert_("Heaney, Seamus, 1939-" in ad.origination)  #whitespace variance
+        self.assertEqual("Manuscript Collection No.653", ad.unitid)
+        self.assertEqual("Manuscript Collection No.653", ad.unitid)
+        self.assertEqual("1 linear ft.(3 boxes)12 oversized papers (OP)", ad.extent)    #FIXME: multiple/list?
+        self.assertEqual("Materials entirely in English.", ad.langmaterial)
+        self.assertEqual("In the Archives.", ad.location)
+        self.assert_(isinstance(ad.access_restriction, Section))
+        self.assertEqual("Restrictions on access", ad.access_restriction.head)
+        self.assert_("Special restrictions apply" in ad.access_restriction.content[0])
+        self.assert_(isinstance(ad.use_restriction, Section))
+        self.assertEqual("Terms Governing Use and Reproduction", ad.use_restriction.head)
+        self.assert_("limitations noted in departmental policies" in ad.use_restriction.content[0])
+        self.assert_(isinstance(ad.alternate_form, Section))
+        self.assertEqual("Publication Note", ad.alternate_form.head)
+        self.assert_("Published in" in ad.alternate_form.content[0])
+        self.assert_(isinstance(ad.originals_location, Section))
+        self.assertEqual("Location of Originals", ad.originals_location.head)
+        self.assert_("Suppressed chapter" in ad.originals_location.content[0])
+        self.assert_(isinstance(ad.related_material, Section))
+        self.assertEqual("Related Materials in This Repository", ad.related_material.head)
+        self.assert_("part of MSS" in ad.related_material.content[0])
+        self.assert_(isinstance(ad.separated_material, Section))
+        self.assertEqual("Related Materials in This Repository", ad.separated_material.head)
+        self.assert_("Ciaran Carson papers, Peter Fallon" in ad.separated_material.content[0])
+        self.assert_(isinstance(ad.acquisition_info, Section))
+        self.assertEqual("Source", ad.acquisition_info.head)
+        self.assert_("Collection assembled from various sources." in ad.acquisition_info.content[0])        
+        self.assert_(isinstance(ad.custodial_history, Section))
+        self.assertEqual("Custodial History", ad.custodial_history.head)
+        self.assert_("Originally received as part of" in ad.custodial_history.content[0])
+        self.assert_(isinstance(ad.preferred_citation, Section))
+        self.assertEqual("Citation", ad.preferred_citation.head)
+        self.assert_("[after identification of item(s)" in ad.preferred_citation.content[0])
+        self.assert_(isinstance(ad.biography_history, Section))
+        self.assertEqual("Biographical Note", ad.biography_history.head)
+        self.assert_("born on April 13" in ad.biography_history.content[0])
+        self.assert_("While at St. Joseph's" in ad.biography_history.content[1])
+        self.assert_(isinstance(ad.bibliography, Section))
+        self.assertEqual("Publication Note", ad.bibliography.head)
+        self.assert_("Susan Jenkins Brown" in ad.bibliography.content[0])
+        self.assert_(isinstance(ad.scope_content, Section))
+        self.assertEqual("Scope and Content Note", ad.scope_content.head)
+        self.assert_("consists of materials relating" in ad.scope_content.content[0])
+        self.assert_(isinstance(ad.arrangement, Section))
+        self.assertEqual("Arrangement Note", ad.arrangement.head)
+        self.assert_("five series" in ad.arrangement.content[0])
+        self.assert_(isinstance(ad.other, Section))
+        self.assertEqual("Finding Aid Note", ad.other.head)
+        self.assert_("Index to selected correspondents" in ad.other.content[0])
+        
+
+    def test_ControlledAccessHeadings(self):
+        ca = self.ead.archdesc.controlaccess
+        self.assert_(isinstance(ca, ControlledAccessHeadings))
+        self.assertEqual("Selected Search Terms", ca.head)
+        self.assert_(isinstance(ca.controlaccess[0], ControlledAccessHeadings))
+        self.assertEqual("Personal Names", ca.controlaccess[0].head)
+        self.assert_(isinstance(ca.controlaccess[0].person_name[0], Heading))
+        self.assertEqual("Barker, Sebastian.", ca.controlaccess[0].person_name[0].value)
+        self.assert_(isinstance(ca.controlaccess[0].family_name[0], Heading))
+        self.assertEqual("Dozier family.", ca.controlaccess[0].family_name[0].value)
+        self.assertEqual("English poetry--Irish authors--20th century.", ca.controlaccess[1].subject[0].value)
+        self.assertEqual("Ireland.", ca.controlaccess[2].geographic_name[0].value)
+        self.assertEqual("Manuscripts.", ca.controlaccess[3].genre_form[0].value)
+        self.assertEqual("Poet.", ca.controlaccess[4].occupation[0].value)
+        self.assert_(isinstance(ca.controlaccess[5].corporate_name[0], Heading))
+        self.assertEqual("Irish Academy of Letters", ca.controlaccess[5].corporate_name[0].value)
+        self.assert_(isinstance(ca.controlaccess[6].function[0], Heading))
+        self.assertEqual("Law enforcing.", ca.controlaccess[6].function[0].value)
+        self.assert_(isinstance(ca.controlaccess[7].title[0], Heading))
+        self.assertEqual("New Yorker (New York, 1925-)", ca.controlaccess[7].title[0].value)
+
+        # terms -  mapps to all types, mixed, in the order they appear
+        all_terms = ca.controlaccess[8].terms
+        self.assertEqual("title", all_terms[0].value)
+        self.assertEqual("person", all_terms[1].value)
+        self.assertEqual("family", all_terms[2].value)
+        self.assertEqual("corp", all_terms[3].value)
+        self.assertEqual("occupation", all_terms[4].value)
+        self.assertEqual("subject", all_terms[5].value)
+        self.assertEqual("geography", all_terms[6].value)
+        self.assertEqual("genre", all_terms[7].value)
+        self.assertEqual("function", all_terms[8].value)
+
+    def test_SubordinateComponents(self):
+        dsc = self.ead.dsc
+        self.assert_(isinstance(dsc, SubordinateComponents))
+        self.assertEqual("combined", dsc.type)
+        self.assertEqual("Description of Series", dsc.head)
+        # c01 - series
+        self.assert_(isinstance(dsc.c[0], Component))
+        self.assertEqual("series", dsc.c[0].level)
+        self.assert_(isinstance(dsc.c[0].did, DescriptiveIdentification))
+        self.assertEqual("Series 1", dsc.c[0].did.unitid)
+        self.assertEqual("Writings by Seamus Heaney", dsc.c[0].did.unittitle)
+        self.assertEqual("Box 1: folders 1-12", dsc.c[0].did.physdesc)
+        # c02 - file
+        self.assert_(isinstance(dsc.c[0].c[0], Component))
+        self.assertEqual("file", dsc.c[0].c[0].level)
+        self.assert_(isinstance(dsc.c[0].c[0].did, DescriptiveIdentification))        
+        self.assert_("holograph manuscript" in dsc.c[0].c[0].did.unittitle)
+        self.assertEqual("box", dsc.c[0].c[0].did.container[0].type)
+        self.assertEqual("1", dsc.c[0].c[0].did.container[0].value)
+        self.assertEqual("folder", dsc.c[0].c[0].did.container[1].type)
+        self.assertEqual("1", dsc.c[0].c[0].did.container[1].value)
+
+        self.assertTrue(dsc.hasSeries())
+        self.assertFalse(dsc.c[0].hasSubseries())
+
+        # second series has a subseries
+        self.assertTrue(dsc.c[1].hasSubseries())
+        # access c03 level item
+        self.assertEqual("file", dsc.c[1].c[0].c[0].level)
+        self.assert_("Hilary Boyle" in dsc.c[1].c[0].c[0].did.unittitle)
+
+    def test_SubordinateComponents_noseries(self):
+        # simple finding aid with no series but only a container list
+        simple_dsc = """<dsc><c01 level="file"/></dsc>"""
+        dsc = load_xmlobject_from_string(simple_dsc, SubordinateComponents)
+        self.assertFalse(dsc.hasSeries())
 
         
 if __name__ == '__main__':
