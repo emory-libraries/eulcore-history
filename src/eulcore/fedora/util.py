1c9a67a9514df0dd802d952eb249029dd3b578d1
Rebecca Sutton Koeser <rsutton@emory.edu>
2010-05-04 22:25:15 +0000

    remove unused function from multipart post recipe
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/trunk@205 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- src/eulcore/fedora/util.py
+++ src/eulcore/fedora/util.py
@@ -23,23 +23,6 @@ Content-Type: %(contenttype)s
 
 """.replace('\n','\r\n')
 
-def post_multipart(host, selector, fields, files):
-    """
-    Post fields and files to an http host as multipart/form-data.
-    fields is a sequence of (name, value) elements for regular form fields.
-    files is a sequence of (name, filename, value) elements for data to be uploaded as files
-    Return the server's response page.
-    """
-    content_type, body = encode_multipart_formdata(fields, files)
-    h = httplib.HTTP(host)
-    h.putrequest('POST', selector)
-    h.putheader('content-type', content_type)
-    h.putheader('content-length', str(len(body)))
-    h.endheaders()
-    h.send(body)
-    errcode, errmsg, headers = h.getreply()
-    return h.file.read()
-
 def encode_multipart_formdata(fields, files):
     """
     fields is a sequence of (name, value) elements for regular form fields.
