87bfb5b23d33635d314cb15afd8d113fc862f504
Rebecca Sutton Koeser <rsutton@emory.edu>
2010-06-11 14:29:36 +0000

    make existdb custom exceptions inherit from existdbexception
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/4suite-to-lxml@310 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- src/eulcore/existdb/exceptions.py
+++ src/eulcore/existdb/exceptions.py
@@ -1,7 +1,9 @@
-class DoesNotExist(Exception):
+from eulcore.existdb.db import ExistDBException
+
+class DoesNotExist(ExistDBException):
     "The query returned no results when exactly one was expected."
     silent_variable_failure = True
 
-class ReturnedMultiple(Exception):
+class ReturnedMultiple(ExistDBException):
     "The query returned multiple results when only one was expected."
     pass
