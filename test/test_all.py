1ff926dc1573b133604bc28b1dabad31d0366af8
Rebecca Sutton Koeser <rsutton@emory.edu>
2010-06-15 19:51:12 +0000

    clean up test_all, update test_django so it works for django 1.2 with and without xmlrunner installed
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/4suite-to-lxml@320 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- test/test_all.py
+++ test/test_all.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import unittest
-import sys
-from django.core.management import setup_environ
+
+from test_django import run_django_tests
 
 # add any non-django modules to be tested here
 non_django_test_modules = (
@@ -17,25 +17,4 @@ def non_django_tests():
         yield unittest.findTestCases(module)
 
 if __name__ == '__main__':
-    # this code inlined (with simplifications) from relevant manage.py code
-
-    # use our settings file to bootstrap the test environment, then switch
-    # to the official one.
-    from django_tester import settings
-    setup_environ(settings)
-    from django.conf import settings
-
-    # FIXME: if we don't import existdb here, starting_tests doesn't get
-    # triggered for some reason
-    from django.test.utils import get_runner
-    from eulcore.django.testsetup import starting_tests, finished_tests
-    import eulcore.django.existdb
-
-    starting_tests.send(None)
-    # in django 1.2, get_runner returns DjangoTestSuiteRunner
-    django_runner = get_runner(settings)(verbosity=1, interactive=True)
-    failures = django_runner.run_tests(test_labels=None,    # run all installed apps
-            verbosity=1, interactive=True, extra_tests=non_django_tests())
-    finished_tests.send(None)
-    if failures:
-        sys.exit(failures)
+    run_django_tests(extras=non_django_tests())
--- test/test_django.py
+++ test/test_django.py
@@ -2,9 +2,14 @@
 
 import sys
 from django.core.management import setup_environ
+from django.test.simple import DjangoTestSuiteRunner
 
-if __name__ == '__main__':
+def run_django_tests(argv=None, extras=None):
     # this code inlined (with simplifications) from relevant manage.py code
+    
+    argv = argv or sys.argv[:]
+    # 0 is script name      (is this always true?)
+    test_apps = argv[1:]    # pass any args as param to run_tests - apps to be tested
 
     # use our settings file to bootstrap the test environment, then switch
     # to the official one.
@@ -19,10 +24,17 @@ if __name__ == '__main__':
     import eulcore.django.existdb
 
     starting_tests.send(None)
-    # in django 1.2, get_runner returns DjangoTestSuiteRunner
-    django_runner = get_runner(settings)(verbosity=1, interactive=True)
-    failures = django_runner.run_tests(test_labels=None)    # run all installed apps
+    django_runner = get_runner(settings)
+    # in django 1.2, if xmlrunner is not installed, get_runner returns DjangoTestSuiteRunner    
+    if django_runner == DjangoTestSuiteRunner:
+        django_runner = django_runner(verbosity=1, interactive=True).run_tests
+
+    failures = django_runner(test_labels=test_apps, interactive=True, 
+                extra_tests=extras)
     finished_tests.send(None)
 
     if failures:
         sys.exit(failures)
+
+if __name__ == '__main__':
+    run_django_tests()
