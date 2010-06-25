c0280254e597df5c82180745c58abc46dfb437c6
Rebecca Sutton Koeser <rsutton@emory.edu>
2010-06-25 22:32:10 +0000

    document & clean-up code for xpath NCName regex
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/4suite-to-lxml@354 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- doc/xpath.rst
+++ doc/xpath.rst
@@ -7,3 +7,16 @@
 -----------------------------------------------------------
 .. automodule:: eulcore.xpath.ast
    :members:
+
+
+Notes
+-----
+
+ * The :mod:`re` standard library module in Python had a bug prior to 2.6.4 that
+   made it reject characters with Unicode characters above U+FFFF. As a result,
+   XPath expressions including these characters in node names, namespace
+   abbreviations, or function names will not work correctly in those versions of
+   Python.
+
+   If you don't know what this means you, almost certainly don't need to worry
+   about it.
--- src/eulcore/xpath/lexrules.py
+++ src/eulcore/xpath/lexrules.py
@@ -4,6 +4,8 @@ To understand how this module works, it is valuable to have a strong
 understanding of the `ply <http://www.dabeaz.com/ply/>` module.
 """
 
+from ply.lex import TOKEN
+
 reserved = {
     'or': 'OR_OP',
     'and': 'AND_OP',
@@ -75,9 +77,48 @@ def t_INTEGER(t):
     t.value = int(t.value)
     return t
 
-    
+# NOTE: some versions of python cannot compile regular expressions that
+# contain unicode characters above U+FFFF, which are allowable in NCNames.
+# These characters can be used in Python 2.6.4, but can NOT be used in 2.6.2
+# (status in 2.6.3 is unknown).  The code below accounts for that and excludes
+# the higher character range if Python can't handle it.
 
+
+# Monster regex derived from:
+#  http://www.w3.org/TR/REC-xml/#NT-NameStartChar
+#  http://www.w3.org/TR/REC-xml/#NT-NameChar
+# EXCEPT:
+# Technically those productions allow ':'. NCName, on the other hand:
+#  http://www.w3.org/TR/REC-xml-names/#NT-NCName
+# explicitly excludes those names that have ':'. We implement this by
+# simply removing ':' from our regexes.
+
+# NameStartChar regex without characters about U+FFFF
+NameStartChar = ur'[A-Z]|_|[a-z]|\xc0-\xd6]|[\xd8-\xf6]|[\xf8-\u02ff]|' + \
+    ur'[\u0370-\u037d]|[\u037f-\u1fff]|[\u200c-\u200d]|[\u2070-\u218f]|' + \
+    ur'[\u2c00-\u2fef]|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]'
+# complete NameStartChar regex
+Full_NameStartChar = ur'(' + NameStartChar + ur'| [\U00010000-\U000EFFFF]' + r')'
+# additional characters allowed in NCNames after the first character
+NameChar_extras = ur'[-.0-9\xb7\u0300-\u036f\u203f-\u2040]'
+
+try:
+    import re
+    # test whether or not re can compile unicode characters above U+FFFF
+    re.compile(ur'[\U00010000-\U00010001]')
+    # if that worked, then use the full ncname regex
+    NameStartChar = Full_NameStartChar
+except:
+    # if compilation failed, leave NameStartChar regex as is, which does not
+    # include the unicode character ranges above U+FFFF
+    pass
+
+NCNAME_REGEX = r'(' + NameStartChar + r')(' + \
+                      NameStartChar + r'|' + NameChar_extras + r')*'
+                      
 NODE_TYPES = set(['comment', 'text', 'processing-instruction', 'node'])
+
+@TOKEN(NCNAME_REGEX)
 def t_NCNAME(t):
 #    ur'[A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c-\u200d\u2070-\u218f\u2c00-\u2fef\u2001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd\U00010000-\U000EFFFF][A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c-\u200d\u2070-\u218f\u2c00-\u2fef\u2001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd\U00010000-\U000EFFFF.0-9\xb7\u0300-\u036f\u203f-\u2040-]*'
 
@@ -92,23 +133,6 @@ def t_NCNAME(t):
         t.type = 'NODETYPE'
     return t
 
-# Monster regex derived from:
-#  http://www.w3.org/TR/REC-xml/#NT-NameStartChar
-#  http://www.w3.org/TR/REC-xml/#NT-NameChar
-# EXCEPT:
-# Technically those productions allow ':'. NCName, on the other hand:
-#  http://www.w3.org/TR/REC-xml-names/#NT-NCName
-# explicitly excludes those names that have ':'. We implement this by
-# simply removing ':' from our regexes.
-#
-# This long line (370 chars!) sucks. There's probably a better way to break
-# this down and still have it work in ply, but I'm missing it.
-try:
-    import re
-    re.compile(ur'[\U00010000-\U00010001]')
-    t_NCNAME.__doc__=ur'[A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c-\u200d\u2070-\u218f\u2c00-\u2fef\u2001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd\U00010000-\U000EFFFF][A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c-\u200d\u2070-\u218f\u2c00-\u2fef\u2001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd\U00010000-\U000EFFFF.0-9\xb7\u0300-\u036f\u203f-\u2040-]*'
-except:
-    t_NCNAME.__doc__=ur'[A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c-\u200d\u2070-\u218f\u2c00-\u2fef\u2001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd][A-Z_a-z\xc0-\xd6\xd8-\xf6\xf8-\u02ff\u0370-\u037d\u037f-\u1fff\u200c-\u200d\u2070-\u218f\u2c00-\u2fef\u2001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd.0-9\xb7\u0300-\u036f\u203f-\u2040-]*'
 
 # Per http://www.w3.org/TR/xpath/#exprlex : 
 #   "If there is a preceding token and the preceding token is not one of @,
