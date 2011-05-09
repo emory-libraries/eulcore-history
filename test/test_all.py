c14d4e17dd9127a750b40b32d531419e56d4c350
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-09 21:14:24 +0000

    add logging config capability to test_all
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/trunk@837 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- test/test_all.py
+++ test/test_all.py
@@ -2,6 +2,8 @@
 
 import os
 import unittest
+import logging.config
+
 from testcore import tests_from_modules
 from test_django import run_django_tests
 
@@ -20,4 +22,9 @@ def non_django_tests():
     return tests_from_modules(non_django_test_modules)
 
 if __name__ == '__main__':
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    LOGGING_CONF = os.path.join(test_dir, 'logging.conf')
+    if os.path.exists(LOGGING_CONF):
+        logging.config.fileConfig(LOGGING_CONF)
+
     run_django_tests(extras=non_django_tests())
