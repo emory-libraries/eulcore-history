6bcac1e539c63dc14b6e4afeb67788e5acc6a304
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-16 20:59:46 +0000

    add pycrypto to install reqs
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/eulfedora@879 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'rdflib>=3.0',
         'python-dateutil',
 	'poster',
+        'pycrypto',
         # soaplib still required for some fedora APIs
         'soaplib==0.8.1',
         # pypi soaplib links are dead as of 20101201. use direct link for
