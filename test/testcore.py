2ff7feea3591c5c038b6e1d75760c8a6b9f9effb
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-16 17:05:22 +0000

    clean up eulxml test suite handling to work without django
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/eulxml@857 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- test/testcore.py
+++ test/testcore.py
@@ -4,10 +4,18 @@ def tests_from_modules(modnames):
     return [ unittest.findTestCases(__import__(modname, fromlist=['*']))
              for modname in modnames ]
 
-def main(testRunner=unittest.TextTestRunner, *args, **kwargs):
+def get_test_runner(runner=unittest.TextTestRunner()):
+    # use xmlrunner if available; otherwise, fall back to text runner
     try:
         import xmlrunner
         testRunner = xmlrunner.XMLTestRunner(output='test-results')
     except ImportError:
         pass
+    return runner
+    
+
+def main(testRunner=None, *args, **kwargs):
+    if testRunner is None:
+        testRunner = get_test_runner()
+
     unittest.main(testRunner=testRunner, *args, **kwargs)
