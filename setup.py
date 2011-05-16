05b0a250971834a15a8dcd68eea0af2821a9566e
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-16 18:13:44 +0000

    remove ply handling in setup.py; comment out eulxml requirement since it apparently causse problems
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/eulexistdb@869 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- setup.py
+++ setup.py
@@ -23,15 +23,7 @@ for path, dirs, files in os.walk(__file__):
     if '__init__.py' in files:
         packages.append(path.replace(os.path.sep, '.'))
 
-class build_py_with_ply(build_py):
-    def run(self, *args, **kwargs):
-        # importing this forces ply to generate parsetab/lextab
-        import eulxml.xpath.core
-        build_py.run(self, *args, **kwargs)
-
 setup(
-    cmdclass={'build_py': build_py_with_ply},
-
     name='eulexistdb',
     version=__version__,
     author='Emory University Libraries',
@@ -39,6 +31,6 @@ setup(
     packages=packages,
     package_dir={'': '.'},
     install_requires=[
-        'eulxml',
+#        'eulxml',
     ],
 )
