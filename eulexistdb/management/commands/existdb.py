db2372aa143309762f55daa25ae62bb5698dcd92
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-17 18:32:25 +0000

    consolidate existdb_index and existdb_reindex into a single manage command
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/eulexistdb@894 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- eulexistdb/management/commands/existdb.py
+++ eulexistdb/management/commands/existdb.py
@@ -1,4 +1,4 @@
-# file eulexistdb/management/commands/existdb_index.py
+# file eulexistdb/management/commands/existdb.py
 # 
 #   Copyright 2010 Emory University General Library
 #
@@ -14,6 +14,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import time
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from eulexistdb.db import ExistDB
@@ -22,12 +23,18 @@ class Command(BaseCommand):
     help = """Tasks for managing eXist-db index configuration file.
 
 Available subcommands:
-  load      - load index configuration file to eXist
-  show      - show the contents of index configuration file currently in eXist
-  show-info - show information about index configuration file in eXist (owner, date modified, etc.)
-  remove    - remove index configuration from eXist
-            """
-    args = 'load | show | show-info | remove'
+  load-index      - load index configuration file to eXist
+  show-index      - show the contents of index configuration file currently in eXist
+  index-info      - show information about index configuration file in eXist (owner, date modified, etc.)
+  remove-index    - remove index configuration from eXist
+  reindex         - reindex the configured eXist collection with the loaded index
+  """
+
+    arg_list = ['load-index', 'show-index', 'index-info', 'remove-index', 'reindex']
+
+    args = ' | '. join(arg_list)
+
+    # FIXME/TODO: possibly convert into a django LabelCommand 
     
     def handle(self, *args, **options):
         if not len(args) or args[0] == 'help':
@@ -35,7 +42,7 @@ Available subcommands:
             return
 
         cmd = args[0]
-        if cmd not in ['load', 'show', 'show-info', 'remove']:
+        if cmd not in self.arg_list:
             print "Command '%s' not recognized" % cmd
             print self.help
             return
@@ -52,17 +59,19 @@ Available subcommands:
         index = settings.EXISTDB_INDEX_CONFIGFILE
 
         try:
-            self.db = ExistDB()
+            # Explicitly request no timeout (even if one is configured
+            # in django settings), since some tasks (such as
+            # reindexing) could take a while.
+            self.db = ExistDB(timeout=None)
 
             # check there is already an index config
             hasindex = self.db.hasCollectionIndex(collection)
 
             # for all commands but load, nothing to do if config collection does not exist
-            if not hasindex and cmd != 'load':
-                print "Collection %s has no index configuration" % collection
-                return
+            if not hasindex and cmd != 'load-index':
+                raise CommandError("Collection %s has no index configuration" % collection)
 
-            if cmd == 'load':
+            if cmd == 'load-index':
                 # load collection index to eXist
 
                 # no easy way to check if index is different, but give some info to user to help indicate
@@ -84,24 +93,43 @@ is new or has changed, you should reindex the collection.
                 else:
                     raise CommandError("Failed to update %s" % message)
 
-            elif cmd == 'show':
+            elif cmd == 'show-index':
                 # show the contents of the the collection index config file in exist
                 print self.db.getDoc(self.db._collectionIndexPath(collection))
 
-            elif cmd == 'show-info':
+            elif cmd == 'index-info':
                 # show information about the collection index config file in exist
                 index_desc = self.db.describeDocument(self.db._collectionIndexPath(collection))
                 for field, val in index_desc.items():
                     print "%s:\t%s" % (field, val)
 
-            elif cmd == 'remove':
+            elif cmd == 'remove-index':
                 # remove any collection index in eXist
                 if self.db.removeCollectionIndex(collection):
                     print "Removed collection index configuration for %s" % collection
                 else:
                     raise CommandError("Failed to remove collection index configuration for %s" % collection)
 
-        except Exception, err:
+
+            elif cmd == 'reindex':
+                # reindex the collection
+                if not self.db.hasCollection(collection):
+                    raise CommandError("Collection %s does not exist" % collection)
+
+                print "Reindexing collection %s" % collection
+                print "-- If you have a large collection, this may take a while."
+                start_time = time.time()
+                success = self.db.reindexCollection(collection)
+                end_time = time.time()
+                if success:
+                    print "Successfully reindexed collection %s" % collection
+                    print "Reindexing took %.2f seconds" % (end_time - start_time)
+                else:
+                    print "Failed to reindexed collection %s" % collection
+                    print "-- Check that the configured exist user is in the exist DBA group."
+
+
+        except Exception as err:
             # better error messages would be nice...
             raise CommandError(err)
 
