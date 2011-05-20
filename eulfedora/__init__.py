3c81e931a2eac25e28d3bc3731dd51fc9f60c208
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-20 20:50:37 +0000

    update license in code, add apache license
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/eulfedora@962 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- eulfedora/__init__.py
+++ eulfedora/__init__.py
@@ -1,6 +1,6 @@
 # file eulfedora/__init__.py
 # 
-#   Copyright 2010 Emory University General Library
+#   Copyright 2010,2011 Emory University Libraries
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,52 +13,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
-# TODO: cleanup/revise documentation here
-
-"""
-:mod:`eulcore.django.fedora` is a Django-aware extension of
-:mod:`eulcore.fedora`.
-
-When you create an instance of :class:`~eulcore.django.fedora.server.Repository`,
-it will automatically configure the repository connection based on Django
-settings, using the configuration names documented below.
-
-If you are writing unit tests that use this module, you should include
-:mod:`eulcore.django.testsetup` in your ``INSTALLED_APPS``.
-:mod:`eulcore.django.fedora` uses the pre- and post- test signals defined
-by :mod:`~eulcore.django.testsetup` to temporarily switch the configured
-fedora root to the test fedora instance. Any :class:`~eulcore.django.fedora.server.Repository`
-instances created within the tests will automatically connect to the test collection.
-If you have a test pidspace configured, that will be used for the default pidspace
-when creating test objects; if you have a pidspace but not a test pidspace,
-the set to use a pidspace of 'yourpidspace-test' for the duration of the tests.
-Any objects in the test pidspace will be removed from the Fedora instance after
-the tests finish.
-
-Projects that use this module should include the following settings in their
-``settings.py``::
-
-    # Fedora Repository settings
-    FEDORA_ROOT = 'http://fedora.host.name:8080/fedora/'
-    FEDORA_USER = 'user'
-    FEDORA_PASSWORD = 'password'
-    FEDORA_PIDSPACE = 'changeme'
-    FEDORA_TEST_ROOT = 'http://fedora.host.name:8180/fedora/'
-    FEDORA_TEST_PIDSPACE = 'testme'
-
-If username and password are not specified, the Repository instance will be
-initialized without credentials.  If pidspace is not specified, the Repository
-will use the default pidspace for the configured Fedora instance.
-
-Projects that need unit test setup and clean-up tasks (syncrepo and
-test object removal) to access Fedora with different credentials than
-the configured Fedora credentials should use the following settings::
-
-    FEDORA_TEST_USER = 'testuser'
-    FEDORA_TEST_PASSWORD = 'testpassword'
-
-"""
 
 
 __version_info__ = (0, 1, 'dev')
