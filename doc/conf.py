3df6b5250e02a922a483a7abe871a1e1eb9e3774
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-20 19:52:35 +0000

    fix django base url for inter-sphinx doc links
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/eullocal@959 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ latex_documents = [
 # configuration for intersphinx: refer to the Python standard library, eulxml, django
 intersphinx_mapping = {
     'http://docs.python.org/': None,
-    'http://docs.djangoproject.com/en/1.3/ref/': 'http://docs.djangoproject.com/en/dev/_objects/',
+    'http://docs.djangoproject.com/en/1.3/': 'http://docs.djangoproject.com/en/dev/_objects/',
     'http://waterhouse.library.emory.edu:8080/hudson/job/eulxml/javadoc/': None,
     'http://waterhouse.library.emory.edu:8080/hudson/job/eulfedora/javadoc/': None,
 #    'http://waterhouse.library.emory.edu:8080/hudson/job/eulexistdb/javadoc/': None,
