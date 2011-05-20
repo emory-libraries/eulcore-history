6e1a7f1738ec126b1dcad7715192c09a8c4bbd26
Rebecca Sutton Koeser <rsutton@emory.edu>
2011-05-20 18:43:50 +0000

    clean up docs for eullocal #33
    git-svn-id: https://svn.library.emory.edu/svn/python-eulcore/branches/eullocal@955 1168706f-7e1d-44e7-b746-9e172b5a9a2f

--- doc/conf.py
+++ doc/conf.py
@@ -1,20 +1,19 @@
 # eulcore documentation build configuration file
 
-import eulcore
+import eullocal
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 
 #templates_path = ['templates']
 exclude_trees = ['build']
 source_suffix = '.rst'
 master_doc = 'index'
 
-project = 'EULcore'
+project = 'EULlocal'
 copyright = '2010, Emory University Libraries'
-version = '%d.%d' % eulcore.__version_info__[:2]
-release = eulcore.__version__
-#modindex_common_prefix = ['eulcore.', 'eulcore.django.']
-modindex_common_prefix = ['eulcore.']
+version = '%d.%d' % eullocal.__version_info__[:2]
+release = eullocal.__version__
+modindex_common_prefix = ['eullocal.']
 
 pygments_style = 'sphinx'
 
@@ -23,6 +22,17 @@ html_style = 'default.css'
 htmlhelp_basename = 'eulcoredoc'
 
 latex_documents = [
-  ('index', 'eulcore.tex', 'EULcore Documentation',
+  ('index', 'eulcore.tex', 'EULlocal Documentation',
    'Emory University Libraries', 'manual'),
 ]
+
+
+# configuration for intersphinx: refer to the Python standard library, eulxml, django
+intersphinx_mapping = {
+    'http://docs.python.org/': None,
+    'http://docs.djangoproject.com/en/1.3/ref/': 'http://docs.djangoproject.com/en/dev/_objects/',
+    'http://waterhouse.library.emory.edu:8080/hudson/job/eulxml/javadoc/': None,
+    'http://waterhouse.library.emory.edu:8080/hudson/job/eulfedora/javadoc/': None,
+#    'http://waterhouse.library.emory.edu:8080/hudson/job/eulexistdb/javadoc/': None,
+#    'http://waterhouse.library.emory.edu:8080/hudson/job/eulcommon/javadoc/': None,
+}
--- doc/index.rst
+++ doc/index.rst
@@ -1,10 +1,14 @@
 EULcore |release|
 =================
 
-EULcore is a collection of reusable Python_ components from `Emory
-University Libraries`_. The library contains both released and unreleased
-beta components. Except where noted otherwise, components documented here
-are released and ready for production use.
+EULlocal is a collection of reusable Python_ components from `Emory
+University Libraries`_. The library contains both released and
+unreleased beta components. Except where noted otherwise, components
+documented here are released and ready for production use.
+
+Modules that were formerly part of :mod:`eulcore` have been split out
+into :mod:`eulxml`, :mod:`eulfedora`, :mod:`eulexistdb`, and
+:mod:`eulcommon`.
 
 .. _Python: http://www.python.org/
 .. _Emory University Libraries: http://web.library.emory.edu/
@@ -16,19 +20,7 @@ Contents
    :maxdepth: 3
    
    changelog
-   xmlmap
-   xpath
-   existdb
    django
-   fedora
-   binfile
-
-----
-
-.. toctree::
-   :maxdepth: 2
-   
-   tutorials/index
 
 
 .. _Dependencies:
@@ -36,7 +28,7 @@ Contents
 Dependencies
 ------------
 
-.. include:: ../src/eulcore/README.dependencies
+.. include:: ../eullocal/README.dependencies
 
 Indices and tables
 ------------------
